f9e3c9295b29c7a2d6486aca48d8677fa3ceab23
Javier Ramos <162994894+JavierRamos1988@users.noreply.github.com>
2024-06-13 19:03:14 +0000

    Firs commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,20 +8,31 @@ const Home = () => {
 	const [ classNameAmarilla, setClassNameAmarilla] = useState("luzAmarillaOff");
 	const [ classNameVerde, setClassNameVerde] = useState("luzVerdeOff");
 	
-	function encenderLuz(){
-		let luzRoja = document.getElementsById("luzRoja");
-		return setClassNameRojo="luzRojaOn"
-	}
+	function encenderLuzRoja(){
+		setClassNameRojo(classNameRojo=> classNameRojo="luzRojaOn");
+		setClassNameAmarilla(classNameAmarilla=> classNameAmarilla="luzAmarillaOff");
+		setClassNameVerde(classNameVerde=> classNameVerde="luzVerdeOff");
+	};
+	function encenderLuzAmarilla(){
+		setClassNameRojo(classNameRojo=> classNameRojo="luzRojaOff");
+		setClassNameAmarilla(classNameAmarilla=> classNameAmarilla="luzAmarillaOn");
+		setClassNameVerde(classNameVerde=> classNameVerde="luzVerdeOff");
+	};
+	function encenderLuzVerde(){
+		setClassNameRojo(classNameRojo=> classNameRojo="luzRojaOff");
+		setClassNameAmarilla(classNameAmarilla=> classNameAmarilla="luzAmarillaOff");
+		setClassNameVerde(classNameVerde=> classNameVerde="luzVerdeOn");
+	};
 
 	return (
 		<div className="contenedor">
 			<div className="palo"></div>
 			<div className="semaforo p-3">
-				<div onClick={()=> setClassNameRojo(classNameRojo=> classNameRojo="luzRojaOn")} id="luzRoja" className={classNameRojo}></div>
-				<div onClick={()=> setClassNameAmarilla(classNameAmarilla=> classNameAmarilla="luzAmarillaOn")} id="luzAmarilla"className={classNameAmarilla}></div>
-				<div onClick={()=> setClassNameVerde(classNameVerde=> classNameVerde="luzVerdeOn")} id="luzVerde"className={classNameVerde}></div>
+				<div onClick={encenderLuzRoja} id="luzRoja" className={classNameRojo}></div>
+				<div onClick={encenderLuzAmarilla} id="luzAmarilla"className={classNameAmarilla}></div>
+				<div onClick={encenderLuzVerde} id="luzVerde"className={classNameVerde}></div>
 			</div>
-			<button onClick={()=> setClassNameRojo(classNameRojo=> classNameRojo="luzRojaOn")} type="button" className="btn btn-primary mt-5">Cambiar Color</button>
+			<button type="button" className="btn btn-primary mt-5">Cambiar Color</button>
 		</div>
 		);
 };
